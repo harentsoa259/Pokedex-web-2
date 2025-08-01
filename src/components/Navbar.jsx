ba877c6402f5d2f0a16dbd620cb5ded88b99ab09
novahMax2552 <hei.vahatra@gmail.com>
2025-08-01 19:41:29 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
-function Navbar() {
+function Navbar({ isDarkMode, toggleDarkMode }) {
   return (
     <nav className="bg-red-500 text-white py-4 shadow-md">
       <div>
         <img src="/src/assets/logo-pokedex.png" alt="" className="ml-7" />
-        </div>
-        <div>
-          
         </div>
     </nav>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,35 +1,44 @@
 import { getTypeGradient } from "./utils/typecolors";
 import Tilt from 'react-parallax-tilt';
 
-function PokemonCard({ pokemon }) {
+function PokemonCard({ pokemon, delay = 0 }) {
   const type = pokemon.types[0]?.type.name;
   const bgColor = getTypeGradient(type);
 
   return (
-      <Tilt glareEnable={true} glareColor="#ffffff" glareMaxOpacity={0.2} scale={1.1}>
-      <div className={`rounded-lg shadow-md p-4 text-white transition hover:scale-105 animate-fade-in hover:shadow-xl hover:shadow-white/20`}
-      style={{ backgroundImage: bgColor }}
-      >
-      <img
-      src={pokemon.sprites.front_default}
-      alt={pokemon.name}
-      className="w-24 mx-auto"
-      />
-      <h2 className="text-xl font-bold text-center mt-2 capitalize">
-        {pokemon.name}
-      </h2>
-      <div className="flex justify-center gap-2 mt-2">
-  {pokemon.types.map(t => (
-    <span
-      key={t.type.name}
-      className="px-2 py-1 text-xs font-semibold rounded-full bg-white/20 backdrop-blur-sm capitalize"
+    <Tilt
+      glareEnable={true}
+      glareColor="#ffffff"
+      glareMaxOpacity={0.2}
+      scale={1.1}
     >
-      {t.type.name}
-    </span>
-  ))}
-</div>
-
-    </div></Tilt>
+      <div
+        className={`rounded-lg shadow-md p-4 text-white transition hover:scale-105 animate-fade-in hover:shadow-xl hover:shadow-white/20`}
+        style={{
+          backgroundImage: bgColor,
+          animationDelay: `${delay}ms`,
+        }}
+      >
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="w-24 mx-auto"
+        />
+        <h2 className="text-xl font-bold text-center mt-2 capitalize">
+          {pokemon.name}
+        </h2>
+        <div className="flex justify-center gap-2 mt-2">
+          {pokemon.types.map(t => (
+            <span
+              key={t.type.name}
+              className="px-2 py-1 text-xs font-semibold rounded-full bg-white/20 backdrop-blur-sm capitalize"
+            >
+              {t.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+    </Tilt>
   );
 }
 
